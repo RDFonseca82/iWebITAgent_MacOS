aef3ae6fe831ca65a857fe02069ba673a4ad2eab
macpc <macpcrdfonseca@gmail.com>
2023-08-10 11:58:01 +0100

    Initial Commit

---
+++ iWebITAgent-macOS/iWebITAgent_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWebITAgent_macOSApp.swift
+//  iWebITAgent-macOS
+//
+//  Created by Admin on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct iWebITAgent_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
