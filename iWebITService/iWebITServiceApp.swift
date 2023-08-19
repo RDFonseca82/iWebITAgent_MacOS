9d72f9a8d9b2cfed101a16910b33e60b10af5663
macpc <macpcrdfonseca@gmail.com>
2023-08-19 17:35:22 +0100

    update

---
+++ iWebITService/iWebITServiceApp.swift
@@ -0,0 +1,19 @@
+//
+//  iWebITServiceApp.swift
+//  iWebITService
+//
+//  Created by Admin on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct iWebITServiceApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
